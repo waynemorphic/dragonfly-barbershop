a2f17ed451f4fab1359186846ea300556daeac28
waynemorphic <wayneklyde@gmail.com>
2023-03-10 17:12:32 +0300

    dragonfly barbershop rough folder structure

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <div>
+            <h3>We are looking to make you <span>handsome</span></h3>
+            <p>You will levae looking sharp, relaxed and ready to 
+                take on the world with that swagger in your stride
+            </p>
+            <button type='submit'>Get Started</button>
+            <img alt='Dragonfly barbershop logo' />
+            <h4>#1 Nairobi Best Barber</h4>
+            <p>Reviews with starts appear here</p>
+        </div>
+        <div>
+            <img alt='Mike Murikui image'/>
+            <p>card with review</p>
+            <p>card with a review appears here</p>
+        </div>
+        
+    </div>
+  )
+}
---
+++ src/pages/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <button>Home</button>
+        <button>About</button>
+        <button>Locations</button>
+        <button>Contact</button>
+        <button>Book Now</button>
+    </div>
+  )
+}
