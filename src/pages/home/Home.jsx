acde4b8785b63e1922131ef2786e52590f259861
waynemorphic <wayneklyde@gmail.com>
2023-03-14 11:20:43 +0300

    home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import '../home/Home.css'
+import shop from '../../assets/shop.jpg'
 
 export default function Home() {
   return (
-    <div>
-        <div>
+    <div className='home'>
+        <div className='home-page'>
             <h3>We are looking to make you <span>handsome</span></h3>
-            <p>You will levae looking sharp, relaxed and ready to 
+            <p>You will leave looking sharp, relaxed and ready to 
                 take on the world with that swagger in your stride
             </p>
             <button type='submit'>Get Started</button>
-            <img alt='Dragonfly barbershop logo' />
+            <img  alt='Dragonfly barbershop logo' />
             <h4>#1 Nairobi Best Barber</h4>
             <p>Reviews with starts appear here</p>
         </div>
-        <div>
-            <img alt='Mike Murikui image'/>
+        <div className='home-page-image'>
+            <img src = {shop} alt='Mike Murikui image'/>
             <p>card with review</p>
             <p>card with a review appears here</p>
         </div>
