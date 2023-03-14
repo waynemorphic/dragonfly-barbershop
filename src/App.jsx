cee1d55600c4a4154ee88ddb37d5bb3d93a9aa90
waynemorphic <wayneklyde@gmail.com>
2023-03-14 11:21:26 +0300

    root

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './pages/home/Home'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Home />
     </div>
   )
 }
