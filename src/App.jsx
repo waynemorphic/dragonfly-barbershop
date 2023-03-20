9390ca6e0e8b00defef8a6aa0831486f48593a3d
waynemorphic <wayneklyde@gmail.com>
2023-03-20 20:43:47 +0300

    components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './pages/home/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Gallery from "./pages/gallery/Gallery";
+import Booking from "./pages/booking/Booking";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "./components/layout/Layout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/booking" element={<Booking />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
