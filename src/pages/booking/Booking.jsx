60deb1e797073d4c535a2131a94240e173153f54
waynemorphic <wayneklyde@gmail.com>
2023-03-23 20:28:32 +0300

    cleanup and destructuring

--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Booking.css";
 import NavBar from "../navbar/NavBar";
+import HairCuts from "./choices/HairCuts";
+import Facials from "./choices/Facials";
+import ManicureAndPedicure from "./choices/ManicureAndPedicure";
+import Cart from "./checkout/Cart";
+import axios from "axios";
 
-const Cart = () => {
-  return (
-    <div>
-      <h3>Cart</h3>
-    </div>
-  );
-};
-
+// navigating between choices
 const Navigator = () => {
   return (
     <div className="navigation">
@@ -19,162 +17,27 @@ const Navigator = () => {
     </div>
   );
 };
-const ManicureAndPedicure = ({ selectedOption }) => {
-  return (
-    <div>
-      <h3>Hands/Feet</h3>
-      <div className="selection" onChange={selectedOption}>
-        <label>
-          <input type="radio" value="800" />
-          Signature Hand Treatment <span>KES 800</span>
-          <br />
-          <p className="time">15 Minutes</p>
-          <p>
-            Quick trimming and shaping of the nails, cuticle care, callus
-            removal and hot towel wrap.
-          </p>
-        </label>
-        <label>
-          <input type="radio" value="800" />
-          Signature Feet Treatment <span>KES 800</span>
-          <br />
-          <p className="time">15 Minutes</p>
-          <p>
-            Quick trimming and shaping of the nails, cuticle care, callus
-            removal and hot towel wrap.
-          </p>
-        </label>
-        <label>
-          <input type="radio" value="1000" />
-          Reventon Hand Treatment <span>KES 1000</span>
-          <br />
-          <p className="time">30 Minutes</p>
-          <p>
-            Trimming and shaping of the nails, cuticle care, callus removal and
-            sea salt skin exfoliation. Hot water soak, massage and hot towel
-            wrap.
-          </p>
-        </label>
-        <label>
-          <input type="radio" value="1200" />
-          Reventon Feet Treatment <span>KES 1200</span>
-          <br />
-          <p className="time">45 Minutes</p>
-          <p>
-            Trimming and shaping of the nails, cuticle care, callus removal and
-            sea salt skin exfoliation. Hot water soak, massage and hot towel
-            wrap.
-          </p>
-        </label>
-      </div>
-    </div>
-  );
-};
-
-const Facials = ({ selectedOption }) => {
-  return (
-    <div>
-      <h3>Facials</h3>
-      <div className="selection" onChange={selectedOption}>
-        <label>
-          <input type="radio" value="500" />
-          Mini Facial <span>KES 500</span>
-          <br />
-          <p className="time">15 Minutes</p>
-          <p>
-            Quick cleanse, coffee & honey exfoliation ,hydrating enzyme masque,
-            hot steamed towel finished with a moisturizer.
-          </p>
-        </label>
-        <label>
-          <input type="radio" value="900" />
-          Signature Facial <span>KES 900</span>
-          <br />
-          <p className="time">30 Minutes</p>
-          <p>
-            Vitamin C cleanse, coffee & honey exfoliation ,hydrating enzyme
-            masque, hot steamed towel finished with a moisturizer.
-          </p>
-        </label>
-        <label>
-          <input type="radio" value="1000" />
-          Steam Facial <span>KES 1000</span>
-          <br />
-          <p className="time">45 Minutes</p>
-          <p>
-            Vitamin C cleanse, coffee & honey exfoliation ,hydrating enzyme
-            masque, steam infusion finished with a moisturizer and a neck &
-            scalp massage.
-          </p>
-        </label>
-      </div>
-    </div>
-  );
-};
-
-const HairCuts = ({ selectedOption, price }) => {
-  return (
-    <div>
-      <h3>Haircuts</h3>
-      <div className="selection" onChange={selectedOption}>
-        <label>
-          <input type="radio" value="1000" />
-          Bespoke Cut <span>KES 1000</span>
-          <br />
-          <p className="time">1 Hr</p>
-          <p>Personalized haircut. Hot towel treatment and styling.</p>
-        </label>
-        <label>
-          <input type="radio" value="1200" />
-          Gentleman's Cut <span>KES 1200</span>
-          <br />
-          <p className="time">1 Hr</p>
-          <p>Classic gentleman's haircut. Hot towel treatment and styling.</p>
-        </label>
-        <label>
-          <input type="radio" value="500" />
-          Beard/Mustache Trim <span>KES 500</span>
-          <br />
-          <p className="time">30 Minutes</p>
-          <p>Personal trim for facial hair.</p>
-        </label>
-        <label>
-          <input type="radio" value="1200" />
-          Ladies Fresh Cut <span>KES 1200</span>
-          <br />
-          <p className="time">1 Hr</p>
-          <p>
-            Personalized haircut for ladies. Hot towel treatment and styling.
-          </p>
-        </label>
-        <label>
-          <input type="radio" value="300" />
-          Eye Brow Trim <span>KES 300</span>
-          <br />
-          <p className="time">1 Hr</p>
-          <p>Personalized eye brows trim.</p>
-        </label>
-        <label>
-          <input type="radio" value="500" />
-          Young Man's Trim <span>KES 500</span>
-          <br />
-          <p className="time">30 Minutes</p>
-          <p>Personalized haircut for kids under 12 yrs.</p>
-        </label>
-      </div>
-    </div>
-  );
-};
 
 export default function Booking() {
-  const [price, setPrice] = useState([]);
+  const [booking, setBooking] = useState([])
+  console.log("booking",booking)
+
+  // fetch data from local json file
+  useEffect(() => {
+    axios
+      .get("/db/db.json")
+      .then((res) => setBooking(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  // const facials = Object.entries(booking)
+  console.log("booking data", booking)
 
   // handle price of selected option
   const handleOptionSelection = (event) => {
-    setPrice((num) => [...num, event.target.value]);
+    setBooking((num) => [...num, event.target.value]);
   };
 
-  console.log("prices", price);
   return (
     <section>
       <NavBar />
@@ -183,9 +46,9 @@ export default function Booking() {
           <Navigator />
         </div>
         <div>
-          <HairCuts selectedOption={handleOptionSelection} />
-          <Facials selectedOption={handleOptionSelection} />
-          <ManicureAndPedicure selectedOption={handleOptionSelection} />
+          <HairCuts selectedOption={handleOptionSelection} booking = {booking} />
+          <Facials selectedOption={handleOptionSelection} booking = {booking} />
+          <ManicureAndPedicure selectedOption={handleOptionSelection} booking = {booking} />
         </div>
         <div>
           <Cart />
