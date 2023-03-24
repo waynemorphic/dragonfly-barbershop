a090fd52ace85225145ea26bbf3cfa1f73c9cfe6
waynemorphic <wayneklyde@gmail.com>
2023-03-24 16:01:28 +0300

    swiper js for carousel with cube effect

--- src/components/bootstrap/ImagesCarousel.jsx
+++ src/components/bootstrap/ImagesCarousel.jsx
@@ -1,30 +1,61 @@
-import  Carousel from "react-bootstrap/Carousel";
-import barbercut from '../../assets/barbercut.png'
-import shop from '../../assets/shop.jpg'
+import barbercut from "../../assets/barbercut.png";
+import shop from "../../assets/shop.jpg";
 
-import './ImagesCarousel.css'
+// Swiper js imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+import { EffectCube, Pagination, Navigation, FreeMode, Autoplay } from "swiper";
 
+import "./ImagesCarousel.css";
 
 export default function ImagesCarousel() {
   return (
-    <Carousel fade = {true} indicators = {false} controls = {true} className="carousel-images">
-        <Carousel.Item>
-            <img className="d-block w-100" src={barbercut} alt="barber shop image 1" />           
-        </Carousel.Item>  
-        `{console.log(Carousel.Item)}`
-
-        <Carousel.Item>
-            <img className = "d-block w-100" src={shop} alt="barber shop image 2"/>
-            <Carousel.Caption>                
-            </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-            <img className = "d-block w-100" src={barbercut} alt="barber shop image 3"/>
-            <Carousel.Caption>
-                
-            </Carousel.Caption>
-        </Carousel.Item>
-    </Carousel>
-  )
+    <Swiper
+      effect={"cube"}
+      slidesPerView = {1}      
+      loop = {true}
+      grabCursor={true}
+      cubeEffect = {{
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94
+      }}
+      freeMode = {true}
+      autoplay = {{delay: 2000}}
+      navigation = {true}
+      pagination = {{clickable: true}}
+      modules={[EffectCube, Pagination, Navigation, FreeMode, Autoplay]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <img
+        className="carousel-images"
+          src={barbercut}
+          alt="barber shop image 1"
+        />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="carousel-images"
+          src={shop}
+          alt="barber shop image 1"
+        />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+        className="carousel-images"
+          src={barbercut}
+          alt="barber shop image 1"
+        />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="carousel-images"
+          src={shop}
+          alt="barber shop image 1"
+        />{" "}
+      </SwiperSlide>
+    </Swiper>   
+  );
 }
