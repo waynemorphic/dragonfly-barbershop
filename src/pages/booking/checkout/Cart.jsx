b1bf4f449203cfbd8210086c83e4286ad8c270f5
waynemorphic <wayneklyde@gmail.com>
2023-03-29 14:13:08 +0300

    mapping all options picked and getting the total cost of options

--- src/pages/booking/checkout/Cart.jsx
+++ src/pages/booking/checkout/Cart.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
 export default function Cart({ prices }) {
-  let totalCost =0;
-  totalCost += parseInt(prices.price);
+  let totalCost = 0;
 
   if (prices.length !== 0) {
     return (
       <div>
         <h3>Cart</h3>
-        {console.log(prices)}
         <ul>
-          <li>{prices.name}: {prices.price}</li>
-  
+          {prices.map((res) => (
+            <li key={res.id}>
+              {res.name}: {res.price}
+            </li>
+          ))}
         </ul>
+        {prices.forEach((element) => {
+          totalCost += element.price;
+        })}
         <p>Total: {totalCost}</p>
 
         <button type="submit">Check Out</button>
