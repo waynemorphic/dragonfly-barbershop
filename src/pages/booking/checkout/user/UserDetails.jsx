d5aeaa7c292c0598c4e55b7ffe9179a25f1c9453
waynemorphic <wayneklyde@gmail.com>
2023-03-31 22:41:03 +0300

    event handlers

--- src/pages/booking/checkout/user/UserDetails.jsx
+++ src/pages/booking/checkout/user/UserDetails.jsx
@@ -5,32 +5,62 @@ import { useState } from "react";
 
 export default function UserDetails({ handleClose, show }) {
    const [value, onChange] = useState(new Date())
+   const [details, setDetails] = useState([])
+   const [fName, setFName] = useState()
+   const [lName, setLName] = useState()
+   const [email, setEmail] = useState()
+   const [phoneNumber, setPhoneNumber] = useState()
+   const [location, setLocation] = useState()
+
+   const handleSubmit = (event) => {
+      event.preventDefault()
+
+      const detailsObject = {
+         fName: fName,
+         lName: lName,
+         email: email,
+         phoneNumber: phoneNumber,
+         location: location
+      }  
+      setDetails(details.concat(detailsObject));
+   }
+
+   const handleFirstname = (event) => setFName(event.target.value)
+
+   const handleLastname = (event) => setLName(event.target.value)
+
+   const handleEmail = (event) => setEmail(event.target.value)
+
+   const handlePhonenumber = (event) => setPhoneNumber(event.target.value)
+
+   const handleLocation = (event) => setLocation(event.target.value)
+
 
   return (
     <div>
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>Enter Your Details</Offcanvas.Header>
         <Offcanvas.Body className="form-area">
-          <form>
+          <form onSubmit={handleSubmit}>
             <label>
               First Name
-              <input required/>
+              <input required onChange={handleFirstname} firstName = {fName} />
             </label>
             <label>
               Last Name
-              <input required/>
+              <input required onChange={handleLastname} lastName = {lName} />
             </label>
             <label>
               Email Address
-              <input type="email" itemType="email" required/>
+              <input type="email" itemType="email" required onChange={handleEmail} email = {email}/>
             </label>
             <label>
               Phone Number
-              <input required placeholder="+254"/>
+              <input required placeholder="+254" onChange={handlePhonenumber} phoneNumber = {phoneNumber} />
             </label>
             <label>
               Location
-              <input required/>
+              <input required onChange={handleLocation} location = {location} />
             </label>            
             <label>
               Date
