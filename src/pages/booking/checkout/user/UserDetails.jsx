d8b1f1d1ad92ba9fda01971caf472a2313e146a9
waynemorphic <wayneklyde@gmail.com>
2023-05-22 18:35:29 +0300

    Fix #2: Replaced React calendar with MUI date pickers

--- src/pages/booking/checkout/user/UserDetails.jsx
+++ src/pages/booking/checkout/user/UserDetails.jsx
@@ -1,37 +1,41 @@
 import Offcanvas from "react-bootstrap/Offcanvas";
 import "./UserDetails.css";
-import Calendar from "react-calendar";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDateTimePicker } from "@mui/x-date-pickers";
 
 export default function UserDetails({
   handleClose,
   show,
-  handleSubmit,
+  // handleSubmit,
+  sendEmail,
   handleFirstname,
   handleLastname,
   handleEmail,
+  refForm,
   handlePhonenumber,
   handleLocation,
   date,
   setDate,
   fname,
-  lname, 
+  lname,
   email,
   phonenumber,
   location
 }) {
   return (
     <div>
-      <Offcanvas show={show} onHide={handleClose}>
+      <Offcanvas show={show} onHide={handleClose} className="form-area">
         <Offcanvas.Header closeButton>Enter Your Details</Offcanvas.Header>
-        <Offcanvas.Body className="form-area">
-          <form onSubmit={handleSubmit}>
+        <Offcanvas.Body >
+          <form onSubmit={sendEmail} ref={refForm}>
             <label>
               First Name
-              <input required onChange={handleFirstname} fname={fname} />
+              <input required onChange={handleFirstname} name="fname" fname={fname} />
             </label>
             <label>
               Last Name
-              <input required onChange={handleLastname} lname={lname} />
+              <input required onChange={handleLastname} name="lname" lname={lname} />
             </label>
             <label>
               Email Address
@@ -40,6 +44,7 @@ export default function UserDetails({
                 required
                 onChange={handleEmail}
                 email={email}
+                name="email"
               />
             </label>
             <label>
@@ -49,20 +54,17 @@ export default function UserDetails({
                 placeholder="+254"
                 onChange={handlePhonenumber}
                 phonenumber={phonenumber}
+                name="phonenumber"
               />
             </label>
             <label>
               Location
-              <input required onChange={handleLocation} location={location} />
+              <input required onChange={handleLocation} name="location" location={location} />
             </label>
             <label>
-              Date
-              <Calendar
-                onChange={setDate}
-                value={date}
-                activeStartDate={new Date()}
-                minDate={new Date()}
-              />
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <StaticDateTimePicker name="date" value={date} onChange={setDate} views={['year', 'month', 'day', 'hours', 'minutes']}/>
+              </LocalizationProvider>
             </label>
             <button type="submit" className="user-button">
               Book Now
