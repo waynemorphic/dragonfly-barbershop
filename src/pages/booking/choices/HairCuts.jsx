ecad137c6c22df5f5688f102c8041e5642fdef48
waynemorphic <wayneklyde@gmail.com>
2023-05-22 11:51:18 +0300

    Fix #1: Haircuts options were not visible. Returned Haircuts options data to the internal json. Data is being fetched from json.

--- src/pages/booking/choices/HairCuts.jsx
+++ src/pages/booking/choices/HairCuts.jsx
@@ -1,50 +1,54 @@
-const hairCutsData = [
-  {
-    value: 1000,
-    description: "Personalized haircut. Hot towel treatment and styling."
-  },
-  {
-    value: 1200,
-    description: "Classic gentleman's haircut. Hot towel treatment and styling."
-  },
-  {
-    value: 500,
-    description: "Personal trim for facial hair."
-  },
-  {
-    value: 1200,
-    description: "Personalized haircut for ladies. Hot towel treatment and styling."
-  },
-  {
-    value:300,
-    description: "Personalized eye brows trim."
-  },
-  {
-    value:500,
-    description: "Personalized haircut for kids under 12 yrs."
-  }
-]
-
-export default function HairCuts({selectedOption, booking}){
-  if (booking.length !== 0){
-    return(
+export default function HairCuts({ selectedOption, booking }) {
+  if (booking.length !== 0)
+    return (
       <div id="#haircuts">
         <h3>Haircuts</h3>
-        <div className="selection" onChangeCapture={selectedOption}>
-        {
-            hairCutsData.map((haircut, index)=> {
-            <label key={index}>
-            <input type="radio" value={haircut.value} name={booking[index].name}/>
-              {booking[index].name}  <span>KES {booking[index].price}</span>          
+        <div className="selection" onChange={selectedOption}>
+          <label>
+            <input type="radio" value="1000" name={booking[0].name} />
+            {booking[0].name} <span>KES {booking[0].price}</span>
             <br />
-            <p className="time">{booking[index].time}</p>
-            <p>{haircut.description}</p>
-            </label>
-          })
-      }     
+            <p className="time">{booking[0].time}</p>
+            <p>Personalized haircut. Hot towel treatment and styling.</p>
+          </label>
+          <label>
+            <input type="radio" value="1200" name={booking[1].name} />
+            {booking[1].name} <span>KES {booking[1].price} </span>
+            <br />
+            <p className="time">{booking[1].time} </p>
+            <p>Classic gentleman's haircut. Hot towel treatment and styling.</p>
+          </label>
+          <label>
+            <input type="radio" value="500" name={booking[2].name} />
+            {booking[2].name} <span>KES {booking[2].price} </span>
+            <br />
+            <p className="time">{booking[2].time}</p>
+            <p>Personal trim for facial hair.</p>
+          </label>
+          <label>
+            <input type="radio" value="1200" name={booking[3].name} />
+            {booking[3].name} <span>KES {booking[3].price}</span>
+            <br />
+            <p className="time">{booking[3].time} </p>
+            <p>
+              Personalized haircut for ladies. Hot towel treatment and styling.
+            </p>
+          </label>
+          <label>
+            <input type="radio" value="300" name={booking[4].name} />
+            {booking[4].name} <span>KES {booking[4].price} </span>
+            <br />
+            <p className="time">{booking[4].time}</p>
+            <p>Personalized eye brows trim.</p>
+          </label>
+          <label>
+            <input type="radio" value="500" name={booking[5].name} />
+            {booking[5].name} <span>KES {booking[5].price} </span>
+            <br />
+            <p className="time">{booking[5].time} </p>
+            <p>Personalized haircut for kids under 12 yrs.</p>
+          </label>
+        </div>
       </div>
-      </div>
-  
-    )
-  }
-} 
+    );
+}
