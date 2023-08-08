ba7a08d4bcf92fc4bf7b932e7acdf0b17ab80ff1
Anubhav <aadhikari1248@gmail.com>
2023-08-08 10:32:11 +0530

    updated the footer section #6

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -3,7 +3,29 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Wayne 2023</p>
+      <p className='wayne'>Wayne 2023</p>
+      <div className='reviews'>
+        <div className='reviews-container'>
+          <h6 className='heading'>Loved the service!!</h6>
+          <p className='name'> John Smith, June 2023</p>
+        </div>
+        <div className='reviews-container'>
+          <h6 className='heading'>Affordable!!</h6>
+          <p className='name'> Mike Helms, July 2023</p>
+        </div>
+        <div className='reviews-container'>
+          <h6 className='heading'>Totally Recommend!</h6>
+          <p className='name'> Philips Helms, July 2023</p>
+        </div>
+        <div className='reviews-container'>
+          <h6 className='heading'>A must visit!</h6>
+          <p className='name'> John Doe, Feb 2023</p>
+        </div>
+        <div className='reviews-container'>
+          <h6 className='heading'>Sanitised tools and barbers</h6>
+          <p className='name'> Chris , Aug 2023</p>
+        </div>
+      </div>
     </div>
   )
 }
