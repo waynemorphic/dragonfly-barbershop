39f1b8da6d845e7849aee388f76d90d4e7de8d0c
Wayne Kirimi <37283450+waynemorphic@users.noreply.github.com>
2025-02-11 14:53:28 +0300

    Update Footer.jsx

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <div className="footer">
-      <Link to="https://github.com/waynemorphic/dragonfly-barbershop">
+      <Link>
         <GithubOutlined />
       </Link>
     </div>
